ad2a285edb0d9f889ae0d1262e2d1fe0462939c8
tangshilan <1147663827@qq.com>
2019-07-07 12:51:44 +0800

    shiro缓存、session配置

--- src/main/java/com/tsl/erp/controller/LoginController.java
+++ src/main/java/com/tsl/erp/controller/LoginController.java
@@ -1,15 +1,15 @@
 package com.tsl.erp.controller;
 
+import com.tsl.erp.shiro.UserRealm;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Auther: Administrator
@@ -21,44 +21,87 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "login";
     }
 
     @RequestMapping("/unAuth")
-    public String unAtuh(){
+    public String unAtuh() {
         return "unAuth";
     }
 
     @PostMapping("/login")
-    public String login(String username, String password, boolean rememberMe,Model model){
+    public String login(String username, String password, boolean rememberMe, Model model) {
         System.out.println("******" + rememberMe);
-        UsernamePasswordToken token = new UsernamePasswordToken(username,password);
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
         token.setRememberMe(rememberMe);
         Subject subject = SecurityUtils.getSubject();
         try {
             subject.login(token);
-        } catch (UnknownAccountException e){
+        } catch (UnknownAccountException e) {
             System.out.println("账户不存在");
-            model.addAttribute("msg","账户不存在");
+            model.addAttribute("msg", "账户不存在");
             return "login";
-        } catch (IncorrectCredentialsException e){
+        } catch (IncorrectCredentialsException e) {
             System.out.println("密码错误");
-            model.addAttribute("msg","密码错误");
+            model.addAttribute("msg", "密码错误");
             return "login";
         }
         return "index";
     }
 
     @RequestMapping("/logout")
-    public String logout(){
+    public String logout() {
         Subject subject = SecurityUtils.getSubject();
         subject.logout();
         return "login";
     }
 
-    @GetMapping({"/","/index"})
-    public String index(){
+    @GetMapping({"/", "/index"})
+    public String index() {
         return "index";
+    }
+
+    /**
+     * 给admin用户添加 userInfo:del 权限
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/addPermission", method = RequestMethod.GET)
+    @ResponseBody
+    public String addPermission(Model model) {
+
+        //在sys_role_permission 表中  将 删除的权限 关联到admin用户所在的角色
+        // roleMapper.addPermission(1,3);
+
+        //添加成功之后 清除缓存
+        DefaultWebSecurityManager securityManager = (DefaultWebSecurityManager) SecurityUtils.getSecurityManager();
+        UserRealm shiroRealm = (UserRealm) securityManager.getRealms().iterator().next();
+        //清除权限 相关的缓存
+        shiroRealm.clearAllCache();
+        return "给admin用户添加 userInfo:del 权限成功";
+
+    }
+
+    /**
+     * 删除admin用户 userInfo:del 权限
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/delPermission", method = RequestMethod.GET)
+    @ResponseBody
+    public String delPermission(Model model) {
+
+        //在sys_role_permission 表中  将 删除的权限 关联到admin用户所在的角色
+        //roleMapper.delPermission(1,3);
+        //添加成功之后 清除缓存
+        DefaultWebSecurityManager securityManager = (DefaultWebSecurityManager) SecurityUtils.getSecurityManager();
+        UserRealm shiroRealm = (UserRealm) securityManager.getRealms().iterator().next();
+        //清除权限 相关的缓存
+        shiroRealm.clearAllCache();
+
+        return "删除admin用户userInfo:del 权限成功";
     }
 }
--- src/main/java/com/tsl/erp/shiro/ShiroConfig.java
+++ src/main/java/com/tsl/erp/shiro/ShiroConfig.java
@@ -1,13 +1,24 @@
 package com.tsl.erp.shiro;
 
 import com.google.common.collect.Maps;
+import org.apache.shiro.cache.ehcache.EhCacheManager;
+import org.apache.shiro.session.SessionListener;
+import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.session.mgt.eis.EnterpriseCacheSessionDAO;
+import org.apache.shiro.session.mgt.eis.JavaUuidSessionIdGenerator;
+import org.apache.shiro.session.mgt.eis.SessionDAO;
+import org.apache.shiro.session.mgt.eis.SessionIdGenerator;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.servlet.SimpleCookie;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.springframework.beans.factory.config.MethodInvokingFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
 
 @Configuration
@@ -39,6 +50,15 @@ public class ShiroConfig {
     @Bean
     public UserRealm userRealm(){
         UserRealm userRealm = new UserRealm();
+        userRealm.setCachingEnabled(true);
+        //启用身份验证缓存，即缓存AuthenticationInfo信息，默认false
+        userRealm.setAuthenticationCachingEnabled(true);
+        //缓存AuthenticationInfo信息的缓存名称 在ehcache-shiro.xml中有对应缓存的配置
+        userRealm.setAuthenticationCacheName("authenticationCache");
+        //启用授权缓存，即缓存AuthorizationInfo信息，默认false
+        userRealm.setAuthorizationCachingEnabled(true);
+        //缓存AuthorizationInfo信息的缓存名称  在ehcache-shiro.xml中有对应缓存的配置
+        userRealm.setAuthorizationCacheName("authorizationCache");
         return  userRealm;
     }
 
@@ -46,8 +66,14 @@ public class ShiroConfig {
     public DefaultWebSecurityManager defaultWebSecurityManager(){
         DefaultWebSecurityManager defaultWebSecurityManager = new DefaultWebSecurityManager();
         defaultWebSecurityManager.setRealm(userRealm());
-        // 使用记住我
+        // 配置rememberMe
         defaultWebSecurityManager.setRememberMeManager(rememberMeManager());
+
+        //配置 ehcache缓存管理器
+        defaultWebSecurityManager.setCacheManager(ehCacheManager());
+
+        //配置自定义session管理，使用ehcache 或redis
+        defaultWebSecurityManager.setSessionManager(sessionManager());
         return defaultWebSecurityManager;
     }
 
@@ -78,5 +104,119 @@ public class ShiroConfig {
         return cookieRememberMeManager;
     }
 
+    /**
+     * shiro缓存管理器;
+     * 需要添加到securityManager中
+     * @return
+     */
+    @Bean
+    public EhCacheManager ehCacheManager(){
+        EhCacheManager cacheManager = new EhCacheManager();
+        cacheManager.setCacheManagerConfigFile("classpath:config/ehcache-shiro.xml");
+        return cacheManager;
+    }
 
+    /**
+     * 让某个实例的某个方法的返回值注入为Bean的实例
+     * Spring静态注入
+     * @return
+     */
+    @Bean
+    public MethodInvokingFactoryBean getMethodInvokingFactoryBean(){
+        MethodInvokingFactoryBean factoryBean = new MethodInvokingFactoryBean();
+        factoryBean.setStaticMethod("org.apache.shiro.SecurityUtils.setSecurityManager");
+        factoryBean.setArguments(new Object[]{defaultWebSecurityManager()});
+        return factoryBean;
+    }
+
+    /**
+     * 配置session监听
+     * @return
+     */
+    @Bean("sessionListener")
+    public ShiroSessionListener sessionListener(){
+        ShiroSessionListener sessionListener = new ShiroSessionListener();
+        return sessionListener;
+    }
+
+    /**
+     * 配置会话ID生成器
+     * @return
+     */
+    @Bean
+    public SessionIdGenerator sessionIdGenerator() {
+        return new JavaUuidSessionIdGenerator();
+    }
+
+    /**
+     * SessionDAO的作用是为Session提供CRUD并进行持久化的一个shiro组件
+     * MemorySessionDAO 直接在内存中进行会话维护
+     * EnterpriseCacheSessionDAO  提供了缓存功能的会话维护，默认情况下使用MapCache实现，内部使用ConcurrentHashMap保存缓存的会话。
+     * @return
+     */
+    @Bean
+    public SessionDAO sessionDAO() {
+        EnterpriseCacheSessionDAO enterpriseCacheSessionDAO = new EnterpriseCacheSessionDAO();
+        //使用ehCacheManager
+        enterpriseCacheSessionDAO.setCacheManager(ehCacheManager());
+        //设置session缓存的名字 默认为 shiro-activeSessionCache
+        enterpriseCacheSessionDAO.setActiveSessionsCacheName("shiro-activeSessionCache");
+        //sessionId生成器
+        enterpriseCacheSessionDAO.setSessionIdGenerator(sessionIdGenerator());
+        return enterpriseCacheSessionDAO;
+    }
+
+    /**
+     * 配置保存sessionId的cookie
+     * 注意：这里的cookie 不是上面的记住我 cookie 记住我需要一个cookie session管理 也需要自己的cookie
+     * @return
+     */
+    @Bean("sessionIdCookie")
+    public SimpleCookie sessionIdCookie(){
+        //这个参数是cookie的名称
+        SimpleCookie simpleCookie = new SimpleCookie("sessionId");
+        //setcookie的httponly属性如果设为true的话，会增加对xss防护的安全系数。它有以下特点：
+
+        //setcookie()的第七个参数
+        //设为true后，只能通过http访问，javascript无法访问
+        //防止xss读取cookie
+        simpleCookie.setHttpOnly(true);
+        simpleCookie.setPath("/");
+        //maxAge=-1表示浏览器关闭时失效此Cookie
+        simpleCookie.setMaxAge(-1);
+        return simpleCookie;
+    }
+
+    /**
+     * 配置会话管理器，设定会话超时及保存
+     * @return
+     */
+    @Bean("sessionManager")
+    public SessionManager sessionManager() {
+
+        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+        Collection<SessionListener> listeners = new ArrayList<SessionListener>();
+        //配置监听
+        listeners.add(sessionListener());
+        sessionManager.setSessionListeners(listeners);
+        sessionManager.setSessionIdCookie(sessionIdCookie());
+        sessionManager.setSessionDAO(sessionDAO());
+        sessionManager.setCacheManager(ehCacheManager());
+
+        //全局会话超时时间（单位毫秒），默认30分钟  暂时设置为10秒钟 用来测试
+        sessionManager.setGlobalSessionTimeout(1800000);
+        //是否开启删除无效的session对象  默认为true
+        sessionManager.setDeleteInvalidSessions(true);
+        //是否开启定时调度器进行检测过期session 默认为true
+        sessionManager.setSessionValidationSchedulerEnabled(true);
+        //设置session失效的扫描时间, 清理用户直接关闭浏览器造成的孤立会话 默认为 1个小时
+        //设置该属性 就不需要设置 ExecutorServiceSessionValidationScheduler 底层也是默认自动调用ExecutorServiceSessionValidationScheduler
+        //暂时设置为 5秒 用来测试
+        sessionManager.setSessionValidationInterval(3600000);
+
+        //取消url 后面的 JSESSIONID
+        sessionManager.setSessionIdUrlRewritingEnabled(false);
+        return sessionManager;
+
+    }
 }
